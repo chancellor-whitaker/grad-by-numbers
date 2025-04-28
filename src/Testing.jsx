3fb1645fc334842a9fca4bf37294e8fde3244835
Chance <chancellor.whitaker@eku.edu>
2025-04-28 12:03:48 -0400

    adding viz data

--- src/Testing.jsx
+++ src/Testing.jsx
@@ -1,11 +1,54 @@
+import { useEffect, useState } from "react";
+import { csv } from "d3-fetch";
+
 import scholar from "./assets/scholar.png";
 import donate from "./assets/donate.png";
 import globe from "./assets/globe.png";
 import star from "./assets/star.png";
 
-const fontSizes = { secondary: 1, tertiary: 4, primary: 6 };
+// creates chart data context
+
+const useVisualizationData = () => {
+  const [filterBy, setFilterBy] = useState([]);
+
+  const noFilterActive = filterBy.length !== 2;
+
+  const data = usePromise(promise);
+
+  const filteredData = noFilterActive
+    ? data
+    : data.filter(({ [filterBy[0]]: value }) => value === filterBy[1]);
+
+  const vizData = getVizData(summarizeData(data));
+
+  const filteredVizData = getVizData(summarizeData(filteredData));
+
+  const showOriginalLabels = (key) => !noFilterActive && key === filterBy[0];
+
+  const handleClick = (...props) => {
+    console.log(props);
+
+    const newPair = [props[0].field, props[0].name];
+
+    setFilterBy((currentPair) => {
+      if (currentPair.length !== 2) return newPair;
+
+      if (currentPair[0] === newPair[0] && currentPair[1] === newPair[1]) {
+        return [];
+      }
+
+      return newPair;
+    });
+  };
+
+  return vizData;
+};
 
 export default function Component() {
+  const data = useVisualizationData();
+
+  console.log(data);
+
   const chartPlaceholder = <div style={{ height: 50 }}>Chart</div>;
 
   const serviceRegionLink = (
@@ -66,13 +109,13 @@ export default function Component() {
           <>
             <Title>{titles.locations}</Title>
             <Metric fontSize={fontSizes.tertiary} lineHeight={1}>
-              13 countries
+              {data.countries.amount} countries
             </Metric>
             <Metric fontSize={fontSizes.tertiary} lineHeight={1}>
-              42 states
+              {data.states.amount} states
             </Metric>
             <Metric fontSize={fontSizes.tertiary} lineHeight={1}>
-              101 counties
+              {data.counties.amount} counties
             </Metric>
           </>
           <ImageBackground className="" height={200}>
@@ -87,14 +130,14 @@ export default function Component() {
           <ImageBackground></ImageBackground>
           <>
             <Title>{titles.age}</Title>
-            <Metric>25</Metric>
+            <Metric>{data.age.average}</Metric>
           </>
           <>
             <div>
-              Min: <span className="text-white">19</span>
+              Min: <span className="text-white">{data.age.min}</span>
             </div>
             <div>
-              Max: <span className="text-white">74</span>
+              Max: <span className="text-white">{data.age.max}</span>
             </div>
           </>
         </HStack>
@@ -105,7 +148,7 @@ export default function Component() {
         <HStack>
           <ImageBackground height={100}></ImageBackground>
           <div className="fw-bold">
-            <Metric as="span">1,538</Metric> {titles.awards}
+            <Metric as="span">{data.awards.amount}</Metric> {titles.awards}
           </div>
           <></>
         </HStack>
@@ -133,7 +176,19 @@ export default function Component() {
           <ImageBackground>{donate}</ImageBackground>
           <>
             <Title>{titles.pell}</Title>
-            <Metric>692</Metric>
+            <Metric>{data.pell.amount}</Metric>
+          </>
+          <></>
+        </HStack>
+      </Block>
+    ),
+    gpa: (
+      <Block>
+        <HStack>
+          <ImageBackground>{star}</ImageBackground>
+          <>
+            <Title>{titles.gpa}</Title>
+            <Metric>{data.gpa.average}</Metric>
           </>
           <></>
         </HStack>
@@ -147,18 +202,6 @@ export default function Component() {
         </Metric>
         <div className="mt-auto"></div>
         {kyStatsSource}
-      </Block>
-    ),
-    gpa: (
-      <Block>
-        <HStack>
-          <ImageBackground>{star}</ImageBackground>
-          <>
-            <Title>{titles.gpa}</Title>
-            <Metric>3.38</Metric>
-          </>
-          <></>
-        </HStack>
       </Block>
     ),
     work: (
@@ -245,7 +288,41 @@ export default function Component() {
   );
 }
 
+const constants = {
+  fontSizes: { secondary: 1, tertiary: 4, primary: 6 },
+  promise: csv("data.csv"),
+};
+
 const helpers = {
+  isStringNumeric: (string) => {
+    const method1 = (value) => !Number.isNaN(value);
+
+    const method2 = (value) => /^[+-]?\d+(\.\d+)?$/.test(value);
+
+    const method3 = (value) => !Number.isNaN(Number(value));
+
+    const method4 = (value) => Number.isFinite(+value);
+
+    const method5 = (value) => value == Number.parseFloat(value);
+
+    return ![method1, method2, method3, method4, method5]
+      .map((method) => method(string))
+      .some((value) => !value);
+  },
+  getChartData: (summary, field, ratio) => {
+    const valueKey = ratio ? "ratio" : "count";
+
+    const object = summary[field]?.values ? summary[field]?.values : {};
+
+    return Object.entries(object)
+      .map(([name, { [valueKey]: value }]) => ({
+        value,
+        field,
+        name,
+      }))
+      .filter(({ name }) => name)
+      .sort(({ value: a }, { value: b }) => b - a);
+  },
   getAspectRatio: (asset) => {
     if (asset === star) return 466 / 445;
 
@@ -255,8 +332,37 @@ const helpers = {
 
     if (asset === globe) return 14 / 17;
   },
+  sumArray: (arr) => {
+    return arr.reduce(
+      (accumulator, currentValue) => accumulator + currentValue,
+      0
+    );
+  },
+  roundToTwo: (num, x = 2) => {
+    return +(Math.round(num + `e+${x}`) + `e-${x}`);
+  },
   getWidth: (asset, height = 75) => {
     return height * getAspectRatio(asset);
+  },
+};
+
+const hooks = {
+  usePromise: (promise) => {
+    const [state, setState] = useState(null);
+
+    useEffect(() => {
+      if (promise) {
+        let ignore = false;
+
+        promise.then((response) => !ignore && setState(response));
+
+        return () => {
+          ignore = true;
+        };
+      }
+    }, [promise]);
+
+    return state;
   },
 };
 
@@ -389,4 +495,146 @@ const components = {
 
 const { ImageBackground, Metric, VStack, HStack, Title, Block } = components;
 
-const { getAspectRatio, getWidth } = helpers;
+const { usePromise } = hooks;
+
+const {
+  isStringNumeric,
+  getAspectRatio,
+  getChartData,
+  roundToTwo,
+  getWidth,
+  sumArray,
+} = helpers;
+
+const { fontSizes, promise } = constants;
+
+const summarizeData = (data) => {
+  const rows = [data].filter((element) => element).flat();
+
+  const object = {};
+
+  rows.forEach((row) => {
+    Object.keys(row).forEach((field) => {
+      if (!(field in object)) {
+        object[field] = { isNumeric: true, distinct: 0, values: {}, sum: 0 };
+      }
+
+      object[field].sum++;
+
+      const value = row[field];
+
+      if (!(value in object[field].values)) {
+        if (!isStringNumeric(`${value}`)) object[field].isNumeric = false;
+
+        object[field].values[value] = 0;
+
+        object[field].distinct++;
+      }
+
+      object[field].values[value]++;
+    });
+  });
+
+  return Object.fromEntries(
+    Object.entries(object).map(
+      ([field, { isNumeric, distinct, values, sum }]) => [
+        field,
+        {
+          average: !isNumeric
+            ? null
+            : sumArray(
+                Object.entries(values).map(
+                  ([value, count]) => Number(value) * count
+                )
+              ) / sum,
+          values: Object.fromEntries(
+            Object.entries(values).map(([value, count]) => [
+              value,
+              { ratio: count / sum, count },
+            ])
+          ),
+          min: !isNumeric
+            ? null
+            : Math.min(
+                ...Object.entries(values).map(([value]) => Number(value))
+              ),
+          max: !isNumeric
+            ? null
+            : Math.max(
+                ...Object.entries(values).map(([value]) => Number(value))
+              ),
+          isNumeric,
+          distinct,
+          sum,
+        },
+      ]
+    )
+  );
+};
+
+const getVizData = (summary) => ({
+  level: {
+    data: getChartData(summary, "SHRDGMR_LEVL_CODE", true).map(
+      ({ value, ...rest }) => ({ value: roundToTwo(value, 4), ...rest })
+    ),
+    field: "SHRDGMR_LEVL_CODE",
+    chart: "pie",
+  },
+  age: {
+    average: Math.floor(summary["Age"]?.average),
+    max: Math.floor(summary["Age"]?.max),
+    min: Math.floor(summary["Age"]?.min),
+    title: "Average Age",
+    field: "Age",
+  },
+  states: {
+    amount: getChartData(summary, "STATENAME").length,
+    data: getChartData(summary, "STATENAME"),
+    field: "STATENAME",
+    title: "States",
+    chart: "bar",
+  },
+  awards: {
+    title: `${summary["acat_desc"]?.sum} Awards`,
+    data: getChartData(summary, "acat_desc"),
+    amount: summary["acat_desc"]?.sum,
+    field: "acat_desc",
+  },
+  pell: {
+    amount: getChartData(summary, "EKU_Low_Inc").find(
+      ({ name }) => name === "Y"
+    )?.value,
+    title: "Pell Recipients",
+    field: "EKU_Low_Inc",
+  },
+  counties: {
+    amount: getChartData(summary, "COUNTYNM").length,
+    data: getChartData(summary, "COUNTYNM"),
+    title: "KY Counties",
+    field: "COUNTYNM",
+  },
+  gpa: {
+    average: roundToTwo(summary["Overall_GPA_End_of_Term"]?.average),
+    field: "Overall_GPA_End_of_Term",
+    title: "Average Graduation GPA",
+  },
+  serviceReg: {
+    data: getChartData(summary, "EKU_Service_Region"),
+    field: "EKU_Service_Region",
+    title: "Service Region",
+  },
+  firstGen: {
+    data: getChartData(summary, "FirstGenInd"),
+    title: "First Generation",
+    field: "FirstGenInd",
+  },
+  majors: {
+    data: getChartData(summary, "major_desc"),
+    field: "major_desc",
+    title: "Majors",
+  },
+  countries: {
+    amount: getChartData(summary, "country").length,
+    field: "country",
+  },
+});
