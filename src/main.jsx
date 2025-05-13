5fade6bca8542f5cfa69b00e2e63d4f925b263eb
Chance <chancellor.whitaker@eku.edu>
2025-05-13 11:27:07 -0400

    html title

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,12 @@ import { StrictMode } from "react";
 import { Wrapper } from "./Wrapper";
 import App from "./App";
 
+// location: https://www.irserver2.eku.edu/Reports/spring-2025-infographic/
+// update 2024 to 2025 in data page reports file
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Wrapper heading="Spring 2025 Grads by the Numbers">
+    <Wrapper heading="Spring 2025 Grads Infographic">
       <App></App>
     </Wrapper>
   </StrictMode>
